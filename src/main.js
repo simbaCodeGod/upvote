92f17487ba0625a6c6dedfb4691d64e0e88f41c1
Simba <isarawadano1@gmail.com>
2023-04-30 14:20:17 +0200

    ls rendering

--- src/main.js
+++ src/main.js
@@ -4,6 +4,21 @@ const upvoteApp = {
             submissions: Seed.submissions
         }
     },
+    computed: {
+        sortedSubmissions() {
+            return this.submissions.sort((a,b) => {
+                return b.votes - a.votes;
+            });
+        }
+    },
+    methods: {
+        upvote(submissionId) {
+            const submission = this.submissions.find(
+                (submission) =>  submission.id === submissionId
+            );
+            submission.votes++;
+        },
+    },
 };
 
 Vue.createApp(upvoteApp).mount('#app');
